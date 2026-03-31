c17413be6c3e7e9f08039cf88ea1e5e4e9432662
thecuong11 <thecuong98lnbg@gmail.com>
2026-04-01 00:38:25 +0700

    inital commit

---
+++ src/test/java/com/fighteasy/FighteasyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fighteasy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FighteasyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
